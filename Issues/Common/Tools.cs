b508ea029f97d39d649416d3291ca53fd40ce39d
constantine17555 <constantine17555@mail.ru>
2025-10-31 13:08:14 +0300

    Add common tools

---
+++ Issues/Common/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Common
+{
+	public class Tools
+	{
+		/// <summary>
+		/// Выполняет функцию и измеряет время выполнения
+		/// </summary>
+		/// <param name="action">Функция</param>
+		/// <returns>Время выполнения</returns>
+		public static TimeSpan MeasureExecution( Action action )
+		{
+			ArgumentNullException.ThrowIfNull( action );
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			action();
+			stopwatch.Stop();
+			return stopwatch.Elapsed;
+		}
+	}
+}
